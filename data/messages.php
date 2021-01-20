fbcdf32dfbaaf8706fe475545db51e8bcf509a7e
Pierre M <69113850+pm1625637@users.noreply.github.com>
2021-01-19 19:08:22 -0500

    Premier ignore

--- data/messages.php
+++ data/messages.php
@@ -2,20 +2,6 @@
 $data[0][0][1]='messages';
 $data[1][0][1]='message';
 $data[1][0][2]='datetime';
-$data[1][1][1]='The table name must be lowercase, plural, contain only alphabetic characters and have a minimum of 4 caracters.';
-$data[1][1][2]='2021-01-18 16:41:03';
-$data[1][2][1]='The table name must be lowercase, plural, contain only alphabetic characters and have a minimum of 4 caracters.';
-$data[1][2][2]='2021-01-18 16:44:39';
-$data[1][3][1]='The table name must be lowercase, plural, contain only alphabetic characters and have a minimum of 4 caracters.';
-$data[1][3][2]='2021-01-18 16:57:25';
-$data[1][4][1]='The table name must be lowercase, plural, contain only alphabetic characters and have a minimum of 4 caracters.';
-$data[1][4][2]='2021-01-18 16:58:36';
-$data[1][5][1]='The table name must be lowercase, plural, contain only alphabetic characters and have a minimum of 4 caracters.';
-$data[1][5][2]='2021-01-18 16:58:47';
-$data[1][6][1]='Primary key is not defined !';
-$data[1][6][2]='2021-01-18 16:59:54';
-$data[1][7][1]='function model::set_line() Real ID of the table or Line is not set.';
-$data[1][7][2]='2021-01-18 17:01:12';
-$data[1][8][1]='Username not found!';
-$data[1][8][2]='2021-01-19 09:47:31';
+$data[1][1][1]='You have initialized &lt;a href="main"&gt;DATPHP 1.0&lt;&#047;a&gt;';
+$data[1][1][2]='2021-01-19 19:06:50';
 ?>
