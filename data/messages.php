89794f0fe20c6b398ef598ce8a605261c3b6da1a
Pierre M <69113850+pm1625637@users.noreply.github.com>
2021-01-20 19:29:19 -0500

    css

--- data/messages.php
+++ data/messages.php
@@ -2,14 +2,12 @@
 $data[0][0][1]='messages';
 $data[1][0][1]='message';
 $data[1][0][2]='datetime';
-$data[1][1][1]='You have initialized';
-$data[1][1][2]='2021-01-20 13:59:10';
-$data[1][2][1]='The file C:&#047;www&#047;BASEController&#047;data&#047;blocks&#047;importation.php does not exist';
-$data[1][2][2]='2021-01-20 14:03:11';
-$data[1][3][1]='You deleted a record from the table: blocks';
-$data[1][3][2]='2021-01-20 14:03:58';
-$data[1][4][1]='Username not found!';
-$data[1][4][2]='2021-01-20 14:22:33';
-$data[1][5][1]='You are logged in!';
-$data[1][5][2]='2021-01-20 14:22:37';
+$data[1][1][1]='Username not found!';
+$data[1][1][2]='2021-01-20 19:24:18';
+$data[1][2][1]='You are logged in!';
+$data[1][2][2]='2021-01-20 19:24:22';
+$data[1][3][1]='You don&#039;t have the right to add a table  in this module.';
+$data[1][3][2]='2021-01-20 19:24:27';
+$data[1][4][1]='You have initialized  Main';
+$data[1][4][2]='2021-01-20 19:25:14';
 ?>
