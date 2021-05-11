a870b921552f7b9274a9c9e5bc39da8ced053012
Pierre M <69113850+pm1625637@users.noreply.github.com>
2021-05-11 13:45:07 -0400

    Api suite

--- controllers/note.php
+++ controllers/note.php
@@ -35,63 +35,7 @@ class Note extends Controller
 		// MAIN PAGE
 		$this->Template->load('layout',$this->data);
 	}
-	function json($url)
-	{
-		$i_table = $this->Get->get_id_table('notes');
-		$i_line = $url[2];
-		$i_column =  $this->Get->get_id_column($i_table,'note');
-		
-		// Only the cell : note
-		$this->data['i_table'] = $i_table;
-		$this->data['i_line'] = $i_line;
-		$this->data['i_column'] = $i_column;
-		$this->data['coord'] = '['.$i_table.']['.$i_line.']['.$i_column.']';
-		$this->data['cell'] =  $this->Get->get_cell($i_table,$i_line,$i_column); 
-		//function get_record($strTable,$line)		
-		$record = $this->Get->get_record('notes',$i_line);	
-		// array(3) { ["id_note"]=> string(1) "1" ["note"]=> string(6) "note 1" ["user_id"]=> string(1) "1" }
-		//$this->data['obj'] = (json_decode(json_encode($record)));
-		$obj = (json_decode(json_encode($record)));
-		//in the view echo $obj->note;
-		$json = json_encode($record);
-		
-		//$this->data['content'] = $this->Template->load('note',$this->data,TRUE);
-		// MAIN PAGE
-		//header('Content-Type:text/plain');
-		//header('Content-Disposition: attachment; filename="note'.$obj->id_note.'.json"');
-		echo (isset($json))? $json:'$json'; 
-
-		//$this->Template->load('json',$this->data);
-	}
-	function api_get($url)
-	{
-		// Headers requis
-		header("Access-Control-Allow-Origin: *");
-		header("Content-Type: application/json; charset=UTF-8");
-		header("Access-Control-Allow-Methods: GET");
-		header("Access-Control-Max-Age: 3600");
-		header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-		// On vérifie que la méthode utilisée est correcte
-		// On vérifie que la clé du user existe : get_field_value_where_unique($strTable,$strColumn,$unique,$strField)
-		$key = $this->Sys->get_field_value_where_unique('users','password',$url[INDEX],'password'); 
-		if( $_SERVER['REQUEST_METHOD'] == 'GET' && $key)
-		{
-				$record = $this->Get->get_record('notes',$url[LINE]);
-
-				// On envoie le code réponse 200 OK
-				http_response_code(200);
-
-				// On encode en json et on envoie
-				echo json_encode($record,JSON_UNESCAPED_UNICODE);
-		}
-		else
-		{
-			// On gère l'erreur
-			http_response_code(405);
-			echo json_encode(["message" => "La méthode n'est pas autorisée"],JSON_UNESCAPED_UNICODE);
-		}
-	}
+	
 	function classes()
 	{
 		$class_methods = get_class_methods('Model');
